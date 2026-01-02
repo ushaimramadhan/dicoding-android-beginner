30ebc9e8852ecd7b2d67a4ac431cfaff71d330b7
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-02 20:16:49 +0700

    feat: create data class and layout item

---
+++ app/src/main/java/com/example/myapplication/Language.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Language(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
